16798ec9ce1e89844359afa02c23e9511d825efd
Timothy Bedsole <timbedsole@gmail.com>
2019-03-21 13:10:46 -0600

    Mapping over each movie in API call and rendering movie poster for each object

--- src/components/movies/MovieList.js
+++ src/components/movies/MovieList.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const API_KEY = process.env.REACT_APP_OMDb_KEY;
 
 class MovieList extends Component {
+  state = {
+    movies: []
+  };
+
+  componentDidMount = () => {
+    axios
+      .get(`http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=Lion`)
+      .then(res => {
+        let movies = res.data.Search;
+        this.setState({ movies });
+        // let moviePosters = movies.map(e => e.Poster);
+        // console.log(this.state);
+      });
+  };
+
   render() {
+    let { movies } = this.state;
+    console.log(movies);
+
     return (
       <div>
         <h1>Movie List</h1>
+        {/* <img src="https://m.media-amazon.com/images/M/MV5BYTYxNGMyZTYtMjE3MS00MzNjLWFjNmYtMDk3N2FmM2JiM2M1XkEyXkFqcGdeQXVyNjY5NDU4NzI@._V1_SX300.jpg" /> */}
+        {movies.map(e => (
+          <img src={e.Poster} alt="Movie poster" />
+        ))}
       </div>
     );
   }
