19b5cd750cd9ffd68c8138e00c3029513c32b7a1
Timothy Bedsole <timbedsole@gmail.com>
2019-03-21 21:30:21 -0600

    Poster images no longer taking up full width, but each image appears to hold its own row. Working on a fix.

--- src/components/movies/MovieList.js
+++ src/components/movies/MovieList.js
@@ -29,16 +29,34 @@ class MovieList extends Component {
       <div>
         <h1>Movie List</h1>
         {/* <img src="https://m.media-amazon.com/images/M/MV5BYTYxNGMyZTYtMjE3MS00MzNjLWFjNmYtMDk3N2FmM2JiM2M1XkEyXkFqcGdeQXVyNjY5NDU4NzI@._V1_SX300.jpg" /> */}
-        {movies.map((movie, index) => (
-          <div className="card">
-            <img
-              className="card-img-top"
-              src={movie.Poster}
-              alt="Movie poster"
-            />
-            <h2 className="card-title text-center">{movie.Title}</h2>
-          </div>
-        ))}
+        {movies.map((movie, index) => {
+          console.log(index);
+          return index % 3 === 0 || index === 0 ? (
+            <div className="row">
+              <div className="col-md-4 mx-auto">
+                <div className="card">
+                  <img
+                    src={movie.Poster}
+                    alt="Movie poster"
+                    className="card-img-top"
+                  />
+                  <h2 className="card-title text-center">{movie.Title}</h2>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className="col-md-4 mx-auto">
+              <div className="card">
+                <img
+                  src={movie.Poster}
+                  alt="Movie poster"
+                  className="card-img-top"
+                />
+                <h2 className="card-title text-center">{movie.Title}</h2>
+              </div>
+            </div>
+          );
+        })}
       </div>
     );
   }
