d3db8405e97d029f7557124d16dd5f9b13805309
Timothy Bedsole <timbedsole@gmail.com>
2019-03-26 17:24:20 -0600

    Wrapped app.js in a provider for redux

--- src/App.js
+++ src/App.js
@@ -6,27 +6,17 @@ import AppSearchBar from './components/layout/AppSearchBar';
 import Header from './components/layout/Header';
 import MovieList from './components/movies/MovieList';
 
-const API_KEY = process.env.REACT_APP_OMDb_KEY;
+import { Provider } from 'react-redux';
+import store from './store';
 
 class App extends Component {
-  //Was testing API endpoint by logging it out when app loads
-  // componentDidMount() {
-  //   axios
-  //     .get(
-  //       `http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&t=The+Big+Lebowski`
-  //     )
-  //     .then(res => {
-  //       console.log(res);
-  //     });
-  // }
-
   render() {
     return (
-      <>
+      <Provider store={store}>
         <Header />
         <AppSearchBar />
         <MovieList />
-      </>
+      </Provider>
     );
   }
 }
