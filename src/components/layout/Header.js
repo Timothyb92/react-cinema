6e3fbbd3a6495f2388ed6c22184a0ba5470566b1
Timothy Bedsole <timbedsole@gmail.com>
2019-04-30 22:31:30 -0600

    added an anchor tag to the 'react cinema' header title to redirect to home. Working on fixing load more movie button after a genre is selected.

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Header = () => {
-  return <div className="text-center">React Cinema</div>;
+  return (
+    <div className="text-center">
+      <a href="/">React Cinema</a>
+    </div>
+  );
 };
 
 export default Header;
