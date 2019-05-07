c502f08700d4c8e4c987c610d2e16d30dd379232
Timothy Bedsole <timbedsole@gmail.com>
2019-05-07 14:47:51 -0600

    Removed bootstrap div and classes from Header.js

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const Header = () => {
-  return (
-    <div className="text-center">
-      <a href="/">React Cinema</a>
-    </div>
-  );
+  return <a href="/">React Cinema</a>;
 };
 
 export default Header;
